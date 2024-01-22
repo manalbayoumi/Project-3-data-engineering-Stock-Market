86a3f1f3374736ef2c87ee5ba41676a8ed6c98ee
manal.bayoumi <manalbayoumi46@gmail.com>
2024-01-21 19:13:11 -0600

    add Picture for Streamlit

--- Groupwebsite.py
+++ Groupwebsite.py
@@ -37,103 +37,48 @@ engine=sqlite3.connect("Group_Project_3.db")
 # Manipulating a web
 st.set_page_config(layout="wide")
 with st.sidebar:
+    buttona=st.button("Introduction")
+    button1=st.button("Installation")
+    button0=st.button("Project Overview")
+    button2=st.button("Original and ETL Data")
+    button3=st.button("Display the Data")
+    button4=st.button("SQLite")
+    button5=st.button("ERD")
     stock=st.radio("select a stock",key="visibility",options=["Apple","Google","Intel","Oracle","Microsoft"])
-    button0= st.button("Introduction")
-    button1=st.button("Orginal and ETL Data")
-    button2=st.button("SQLite")
-    button3=st.button("ERD")
-    button4=st.button("Display the Data")
-    button5=st.button("Plotting the Data")
-    button6=st.button("Plotting the Candle")
-    button7=st.button("About the Group")
-    button8=st.button("Conclusion")
-    button9=st.button("References")
-
+    button6=st.button("Plotting the Data")
+    button8=st.button("About the Group")
+    button9=st.button("Conclusion")
+ 
 
 st.title(":green[Project 3] :violet[Data] :blue[Engineering]")
 st.write("---")
 
-if button0: 
+if buttona:
+    st.image("./Web/data.jpg")
+
+elif button0: 
     intro="""
-    # Project Overview: 
-### Our project will analyze historical stock data for five companies:    
-*  :blue[Alphabet Inc. (aka Google)]
-* :red[Apple Inc.]
-* :violet[Intel Corporation]
-* :green[Microsoft Corporation]
-* :orange[Oracle Corporation]   
+     
+## Our project will analyze historical stock data for five companies:
+#
+#
 
 """
-    st.write(intro)
-elif button1:
-    st.write("# Original Data")
-    st.image("./Web/before_ETL.png")
-    st.write("---")
-
-    st.write("# Transformed Data")
-    st.image("./Web/after_ETL.png")
-    text="""
-    To make it more efficent for analysing, use `ETL` to transform data, including
-* Adding addtional column
-   * stock_ticker: for easily identify stocks.
-   * normalization: for comparing different stock gain and loss.
-   * percentage_change: to determine which is the best and worst day to invest.
-   * accumulative_return: to determine how investor get return from the market.
-* Changing datatype for database to record.
-* remove redundant floating point. 
-"""
-    st.write(text)
-elif button2:
-    st.write("# SQLite Tables")
-    st.image("./Web/Sqlite_table.PNG")
-    st.write("# Apple Stock")
-    st.image("./Web/Sql_Apple.PNG")
+    st.write(intro)    
+    col1,col2,col3,col4, col5=st.columns(5)
+    with col1:
+        st.image("./Web/Google.png")
+    with col2:
+        st.image("./Web/Apple.png") 
+    with col3:
+        st.image("./Web/intel.png")
+    with col4:
+        st.image("./Web/Microsoft.jpg",width=180)
+    with col5:
+        st.image("./Web/Oracle.png")
     
-elif button3:   
-    st.write("# ERD")
-    st.image("./Web/ERDdiagram.jpg")
 
-
-elif button4: 
-    temp=stock+"_price"
-    df=Database_to_Frame(temp)
-    st.write(df)
-
-elif button5: 
-    temp=stock+"_price"
-    df=Database_to_Frame(temp)
-    fig=bubble(df)
-    st.plotly_chart(fig,use_container_width=True)
-
-elif button6: 
-    temp=stock+"_price"
-    df=Database_to_Frame(temp)
-    fig=candleplot(df, stock)
-    st.plotly_chart(fig,use_container_width=True)
-
-elif button7: 
-    st.write(":green[Manal Bayoumi]")
-    st.write(":red[Melissa Mosby]")
-    st.write(":blue[Rocio Cantu]")
-
-elif button8: 
-    message="""
-
-## That project will determine various questions when analyzing these stocks:
-* How do investors pick up their stocks?   
-They analyze the data, check the daily price for the stock. They try to understand the company's business before choosing a stock.
-* In buying and selling stocks, do investors depend on the opening or the closing prices of stocks?
-* What factors make stock prices rise or fall for a company like Apple, Google, Intel, Oracle, Microsoft?
-
-
-* Our analysis will follow a data engineering track. We will employ jupyter notebook and pandas to capture and modify our data using ELT workflows. 
-Next, we will feed our data into SQL. Also, we will generate an ERD diagram to show the relationships of our database tables. 
-We will create a method to read our data from the database with pandas DataFrame. 
-
-"""
-    st.write(message)
-
-elif button9: 
+elif button1: 
     message="""
     ### Required downloaded package
     *Please run the following command in jupyter or bash terminal*
@@ -152,7 +97,80 @@ streamlit run Groupwebsite.py
 """
     st.write(message)
 
-else: st.image("title.png")
+elif button2:
+    st.write("# Original Data")
+    st.image("./Web/before_ETL.png")
+    st.write("---")
+
+    st.write("# Transformed Data")
+    st.image("./Web/after_ETL.png")
+    text="""
+    To make it more efficent for analyzing, use `ETL` to transform data, including:
+* Adding the following column
+   * stock_ticker: for easily identify stocks.
+   * normalization: for comparing different stock gain and loss.
+   * percentage_change: to determine which is the best and worst day to invest.
+   * accumulative_return: to determine how investor get return from the market.
+   * Changing datatype for database to record.
+   * removing redundant floating point. 
+
+"""
+  
+    st.write(text)
+
+elif button3: 
+    temp=stock+"_price"
+    df=Database_to_Frame(temp)
+    st.write(df)
+
+elif button4:
+    st.write("# SQLite Tables")
+    st.image("./Web/Sqlite_table.PNG")
+    st.write("# Apple Stock")
+    st.image("./Web/Sql_Apple.PNG")
+    
+elif button5:   
+    st.write("# ERD")
+    st.image("./Web/ERDdiagram.jpg")
+
+
+elif button6: 
+    temp=stock+"_price"
+    df=Database_to_Frame(temp)
+    fig=bubble(df)
+    st.plotly_chart(fig,use_container_width=True)
+    fig=candleplot(df, stock)
+    st.plotly_chart(fig,use_container_width=True)
+
+
+elif button8:
+    col1,col2,col3=st.columns(3)
+    with col1:
+        st.write("# Melissa Mosby")
+        st.image("./Web/Melissa_picture_8272.jpg")
+    with col2:
+        st.write("# Rocio Cantu")
+        st.image("./Web/Rocio_picture_2903.jpg") 
+    with col3:
+        st.write("# Manal Bayoumi")
+        st.image("./Web/Manal_picture_7035.jpg")
+
+elif button9: 
+    message="""
+
+# List of possible questions in the next data analysis step:
+* How do investors pick their stocks?   
+* In buying and selling stocks, do investors depend on the opening or the closing prices of stocks?
+* What factors make stock prices rise or fall for a company like Apple, Google, Intel, Oracle, Microsoft?
+* What makes an investor pick one company's stock over another?
+
+"""
+
+    st.write(message)
+
+
+
+
 
 
     
